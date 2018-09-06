ad8d470bad6f0583a704df20c1b9b74530c4d306
bhaskargoel <bhaskargoel12@gmail.com>
2018-09-05 20:30:34 -0700

    Update and rename LICENSE.md to diamond.py

---
+++ LICENSE.md/diamond.py
@@ -0,0 +1,11 @@
+n= 9 
+a = 1
+b = 1
+for a in range(n+1):
+    if a%2 != 0:
+        val = (n - a) // 2
+        print (" "*val + "*"*(a) + " "*val ,end = "\n")
+for b in range(n-1,0,-1):
+    if b%2 != 0:
+        val2 = (n-b)//2
+        print (" "*val2 + "*"*(b) + " "*val2 ,end = "\n")
