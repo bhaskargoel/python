4bd5c28579cfcacaeefb536ddb44ff2187ad1d0a
bhaskargoel <bhaskargoel12@gmail.com>
2018-09-05 20:33:56 -0700

    Add files via upload

---
+++ diamond.py
@@ -0,0 +1,18 @@
+k = 0
+rows = 5
+for i in range(1, rows+1):
+    for space in range(1, (rows-i)+1):
+        print(end="  ")
+    while k != (2*i-1):
+        print("* ", end="")
+        k = k + 1
+    k = 0
+    print()
+for j in range(rows-1, 1, -1)
+    for space in range(1, (rows-i)+1):
+        print(end="  ")
+    while k != (2*i-1):
+        print("* ", end="")
+        k = k + 1
+    k = 0
+    print()
