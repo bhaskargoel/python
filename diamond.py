84437201cb248976d13d12e055b78224551acc2e
bhaskargoel <bhaskargoel12@gmail.com>
2018-09-05 20:34:58 -0700

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -1,18 +1,11 @@
-k = 0
-rows = 5
-for i in range(1, rows+1):
-    for space in range(1, (rows-i)+1):
-        print(end="  ")
-    while k != (2*i-1):
-        print("* ", end="")
-        k = k + 1
-    k = 0
-    print()
-for j in range(rows-1, 1, -1)
-    for space in range(1, (rows-i)+1):
-        print(end="  ")
-    while k != (2*i-1):
-        print("* ", end="")
-        k = k + 1
-    k = 0
-    print()
+n= 9 
+a = 1
+b = 1
+for a in range(n+1):
+    if a%2 != 0:
+        val = (n - a) // 2
+        print (" "*val + "*"*(a) + " "*val ,end = "\n")
+for b in range(n-1,0,-1):
+    if b%2 != 0:
+        val2 = (n-b)//2
+        print (" "*val2 + "*"*(b) + " "*val2 ,end = "\n")
